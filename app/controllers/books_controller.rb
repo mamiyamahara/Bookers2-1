38c72b8d2d3d87a215d26afb42db1378fad40ee4
vagrant <vagrant@localhost.localdomain>
2020-08-03 06:22:01 +0000

    [add]部分テンプレート、Bootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,10 +16,14 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
   
   def show
     @books = Book.all
+    @book = Book.new
+    @user = current_user
+
   end
 
   def edit
@@ -29,6 +33,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
     else
       render :edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
+    @user = current_user
+    @book = Book.new
   end
 
   def edit
